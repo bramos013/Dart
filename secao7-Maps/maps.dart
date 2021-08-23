00c2b7f6b43bda7349fe1f72189e7edfa587fd80
bramos013 <brunoramos013@gmail.com>
2021-08-23 00:47:32 -0300

    Trabalhando com Maps

---
+++ secao7-Maps/maps.dart
@@ -0,0 +1,15 @@
+main() {
+  var maps = {
+    "Nome": "Bruno Ramos",
+    "Idade": 28,
+    "Cidade": "Porto Alegre",
+    "Estado": "RS"
+  };
+
+  print(maps);
+
+  maps["Nome"] = "Benício Freitas";
+  maps["Idade"] = 0;
+
+  print(maps);
+}
